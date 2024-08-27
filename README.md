ecff9cf2c44331f338ca73ff3c5eef1387cc1613
rohan deshpnde <rohandeshpande1632@gmail.com>
2024-08-27 17:49:37 +0530

    final

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## ML FLOW EXPERIMENTS
+
+MLFLOW_TRACKING_URI="https://dagshub.com/rrd1997/mlflowexperiment.mlflow"
+MLFLOW_TRACKING_USERNAME=rrd1997 \
+MLFLOW_TRACKING_PASSWORD=81e89980b25b43cbaaabe283d1a54de5 \
+python script.py
--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
 import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
@@ -26,14 +27,9 @@ if __name__ == "__main__":
     
     data = pd.read_csv(r"C:\Users\Shree\Desktop\MLProject\mlops\winequality-red.csv")
 
-    data.columns = data.columns.str.strip()
-
-    # Check if 'quality' column exists
-    if 'quality' not in data.columns:
-        raise KeyError("'quality' column is missing from the dataset")
 
     # Split the data
-    train, test = train_test_split(data, test_size=0.2, random_state=42)
+    train, test = train_test_split(data)
 
     # Separate features and target variable
     train_x = train.drop("quality", axis=1)
@@ -51,7 +47,7 @@ if __name__ == "__main__":
         lr.fit(train_x, train_y)
 
         predicted_qualities = lr.predict(test_x)
-        rmse, mae, r2 = eval_metrics(test_y, predicted_qualities)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
         print("ElasticNet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
         print("  RMSE: %s " % rmse)
@@ -61,9 +57,23 @@ if __name__ == "__main__":
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("mae", mae)
         mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
 
         # Create model signature
-        signature = infer_signature(train_x, lr.predict(train_x))
-        mlflow.sklearn.log_model(lr, "model", signature=signature)
+        predictions=lr.predict(train_x)
+        signature = infer_signature(train_x, predictions)
+        
+
+    ## for remote server 
+    remote_server_uri="https://dagshub.com/rrd1997/mlflowexperiment.mlflow"
+    mlflow.set_tracking_uri(remote_server_uri)
+    
+
+    tracking_url_type_store= urlparse(mlflow.get_tracking_uri()).scheme
+
+    if tracking_url_type_store !="file":
+        mlflow.sklearn.log_model(
+        lr,"model",registered_model_name="ElasticnetwineModel",signature=signature)
+    else:
+        mlflow.sklearn.load_model(lr,"model",signature=signature)
